c9099dea46ee38c4da3794e7ab281f83c5e42a1e
Oleg <olegdemchenko1993@gmail.com>
2022-02-15 17:45:22 +0200

    Implement navigation to issue details

--- src/Components/Issues.jsx
+++ src/Components/Issues.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import IssuesWrapper from './IssuesWrapper';
+import IssueDetails from './IssueDetails';
 import FilteringCriteria from './FilteringCriteria';
 import IssuesList from './IssuesList';
 
@@ -33,13 +34,24 @@ const sortIssues = (order, issues) => {
 };
 
 const Issues = ({ issues }) => {
+  const initialRenderInfo = { render: "list" };
+  const [renderInfo, setRenderInfo] = useState(initialRenderInfo);
   const initCriteria = {
     labelFilter: '',
     assigneeFilter: '',
     sortOrder: 'none',
   };
   const [currentCriteria, setCriteria] = useState(initCriteria);
-
+  const showIssueDetails = (issue) => setRenderInfo({ render: "details", item: issue });
+  const showIssuesList = () => setRenderInfo(initialRenderInfo);
+  
+  if (renderInfo.render === "details") {
+    return (
+      <IssuesWrapper>
+        <IssueDetails issue={renderInfo.item} goBack={showIssuesList} />
+      </IssuesWrapper>
+    )
+  }
   const changeCriteria = (type, value) => {
     setCriteria((prevCriteria) => ({ ...prevCriteria, [type]: value }));
   };
@@ -54,7 +66,7 @@ const Issues = ({ issues }) => {
         criteria={currentCriteria}
         changeCriteria={changeCriteria}
       />
-      <IssuesList issues={sortedIssues} />
+      <IssuesList issues={sortedIssues} showDetails={showIssueDetails}/>
     </IssuesWrapper>
   );
 };
--- src/Components/IssuesList.jsx
+++ src/Components/IssuesList.jsx
@@ -1,21 +1,22 @@
 import { Row, Col, Card, ListGroup } from "react-bootstrap";
 
-const renderIssue = ({ title, labels, assignee, comments, id }) => {
-  return (
-    <Col key={id}>
-      <Card className="mb-4 btn btn-outline-dark" border="light">
-        <h5 className="fw-bolder px-1 py-1">{title}</h5>
-        <ListGroup className="rounded" variant="flush">
-          <ListGroup.Item>{labels.join('\n')}</ListGroup.Item>
-          <ListGroup.Item>{assignee ?? "Not assigned"}</ListGroup.Item>
-          <ListGroup.Item>{comments} comments</ListGroup.Item>
-        </ListGroup>
-      </Card>
-    </Col>
-  );
-};
+const IssuesList = ({ issues, showDetails }) => {
+  console.log(showDetails);
+  const renderIssue = (issue) => {
+    return (
+      <Col key={issue.id} onClick={() => showDetails(issue)}>
+        <Card className="mb-4 btn btn-outline-dark" border="light">
+          <h5 className="fw-bolder px-1 py-1">{issue.title}</h5>
+          <ListGroup className="rounded" variant="flush">
+            <ListGroup.Item>{issue.labels.join('\n')}</ListGroup.Item>
+            <ListGroup.Item>{issue.assignee ?? "Not assigned"}</ListGroup.Item>
+            <ListGroup.Item>{issue.comments} comments</ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    );
+  };
 
-const IssuesList = ({ issues }) => {
   return (
     <Row xs="1" md="2" xl="3" className="g-4 border-dark border-top">
       {issues.map((issue) => renderIssue(issue))}
