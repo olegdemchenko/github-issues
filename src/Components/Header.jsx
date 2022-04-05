bc69d7ef421d7af3d480744a21353bd34e327192
Oleg <olegdemchenko1993@gmail.com>
2022-04-05 15:06:19 +0300

    Delete unnecessary console.log from Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Navbar, Container, Button } from "react-bootstrap";
 
 const Header = ({ isSignedIn, signOut }) => {
-  console.log();
   return (
     <Navbar className="shadow" variant="light" bg="white" expand="lg">
       <Container>
